da67bfd7504dad25c830e1212fdcfde805f2d4f0
Brandon Cowley <brandon.cowley@gmail.com>
2021-08-08 15:51:00 -0700

    Adding HTML routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const db = require("../models")
+const path = require("path");
+
+module.exports = (app) => {
+//get the index
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public.index.html'));
+});
+//get the exercises
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public.exercise.html'));
+});
+//get the stats
+app.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public.stats.html'));
+});
+}
