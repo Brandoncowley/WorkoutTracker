5dcf6f2f18be3d1bbd18229cd27e597c86a626f9
Brandon Cowley <brandon.cowley@gmail.com>
2021-08-08 17:52:56 -0700

    fixed routes for HTML

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,14 +4,14 @@ const path = require("path");
 module.exports = (app) => {
 //get the index
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public.index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 //get the exercises
 app.get('/exercise', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public.exercise.html'));
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
 });
 //get the stats
 app.get('/stats', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public.stats.html'));
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
 });
 }
