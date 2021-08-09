45fcafb1c3ef696e6c38bc56927ed925aaededd9
Brandon Cowley <brandon.cowley@gmail.com>
2021-08-08 21:11:32 -0700

    Fixed my routes, corrected model error to display total workout duration, removed null object being created

--- models/workout.js
+++ models/workout.js
@@ -1,22 +1,40 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-const WorkoutSchema = new Schema ({
+const WorkoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now()
     },
-    exercises: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
+    
+    exercises: [{
+        type: {
+            type: String,
+            required: 'Enter an exercise type'
+        },
+        name: {
+            type: String,
+            required: 'Enter exercise name'
+        },
+        duration: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
         }
-    ],
+    }],
     totalDuration: {
         type: Number,
-        default: 0
-    }
+        default: 0,
+      }
 });
 
 const Workout = mongoose.model("workout", WorkoutSchema);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,22 +3,34 @@ module.exports = (app) => {
 
     //get all workouts
     app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}, (err, workouts) => {
-            if(err){
-                console.log(err);
-            } else {
-                res.json(workouts)
-            }
+        db.Workout.find({}).then(dbWorkout => {
+            dbWorkout.forEach(workout => {
+                var total = 0;
+                workout.exercises.forEach(e => {
+                        total += e.duration;
+                });
+                workout.totalDuration = total;
+            });
+    
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
         });
     });
+
     //add excerise, give an id, push to model, set true
-    app.put("/api/workouts/:workout", ({ params, body }, res) => {
-        db.Workout.findOneAndUpdate({ _id: params.id},
-                                    {$push: {excercises:body }},
-                                    { upsert: true, useFindandModify:false},
-                                    updatedWorkout => {
-                                        res.json(updatedWorkout);
-                                    })
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $inc: { totalDuration: req.body.duration },
+                $push: { exercises: req.body }
+            },
+            { new: true }).then(dbWorkout => {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
     });
     //create new workout - important!
     app.post('/api/workouts', (req,res) => {
@@ -27,4 +39,19 @@ module.exports = (app) => {
         });
     });
 
+    app.get('/api/workouts/range', (req, res) => {
+        db.Workout.aggregate([{
+            $addFields:{
+                totalDuration:{
+                    $sum:'$exercises.duration'
+                }
+            }
+        }]).then ((workouts) => {
+            res.json(workouts)
+        }).catch((err) => {
+            res.json(err)
+        })
+    });
+
 }
+
