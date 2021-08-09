13c85fdbd622734200df8d61bf287cb05f41caf7
Brandon Cowley <brandon.cowley@gmail.com>
2021-08-08 21:28:40 -0700

    trying to connect updated code

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static("public"));
 
 // Adding new connection code for Heroku
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/WorkoutDB',
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
